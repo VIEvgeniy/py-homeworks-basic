ecc3050098cfa3e2561af3c7b0d9f05765798ad5
Evgeniy Vakulenko <vakevg@yandex.ru>
2022-10-04 17:24:56 +0300

    add __str__ to students_and_mentor.py

--- 6.classes/students_and_mentor.py
+++ 6.classes/students_and_mentor.py
@@ -8,13 +8,27 @@ class Student:
         self.grades = {}
 
     def rate_hw(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress and 0 <= grade <= 10:
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress and 0 <= grade <= 10:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def average_grades(self):
+        all_grades = list(sum(self.grades.values(), []))
+        return sum(all_grades) / len(all_grades)
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\n' \
+              f'Средняя оценка за домашние задания: {self.average_grades()}\n' \
+              f'Курсы в процессе изучения: ' + \
+              ', '.join(self.courses_in_progress) + \
+              f'\nЗавершенные курсы: ' + \
+              ', '.join(self.finished_courses)
+        return res
 
 
 class Mentor:
@@ -23,11 +37,22 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return res
+
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def average_grades(self):
+        return Student.average_grades(self)
+
+    def __str__(self):
+        res = super().__str__() + f'\nСредняя оценка за лекции: {self.average_grades()}'
+        return res
 
 
 class Reviewer(Mentor):
@@ -41,20 +66,25 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-best_student = Student('Evgeniy', 'Vakulenko', 'your_gender')
-best_student.courses_in_progress += ['Python']
+some_student = Student('Ruoy', 'Eman', 'your_gender')
+some_student.courses_in_progress += ['Python']
+some_student.courses_in_progress += ['Git']
+some_student.finished_courses += ['Введение в программирование']
 
-some_lecturer = Lecturer('Ivan', 'Ivanov')
+some_lecturer = Lecturer('Some', 'Buddy')
 some_lecturer.courses_attached += ['Python']
 
 some_reviewer = Reviewer('Some', 'Buddy')
 some_reviewer.courses_attached += ['Python']
+some_reviewer.courses_attached += ['Git']
 
-some_reviewer.rate_hw(best_student, 'Python', 10)
-some_reviewer.rate_hw(best_student, 'Python', 10)
-some_reviewer.rate_hw(best_student, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Python', 9)
+some_reviewer.rate_hw(some_student, 'Git', 10)
 
-best_student.rate_hw(best_lecturer, 'Python', 10)
+some_student.rate_hw(some_lecturer, 'Python', 10)
 
-print(f'Оценки студента {best_student.name} {best_student.surname}: {best_student.grades}')
-print(f'Оценки лектора {best_lecturer.name} {best_lecturer.surname}: {best_lecturer.grades}')
+print(some_reviewer)
+print(some_lecturer)
+print(some_student)
