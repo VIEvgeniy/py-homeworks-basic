ca74a7b577a7dcf924f9bda6214187f9ae3c3d02
Evgeniy Vakulenko <vakevg@yandex.ru>
2022-10-04 14:45:10 +0300

    add class Lecture

--- 6.classes/students_and_mentor.py
+++ 6.classes/students_and_mentor.py
@@ -6,13 +6,31 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress and 0 <= grade <= 10:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -22,20 +40,21 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass
 
-class Reviewer(Mentor):
-    pass
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Evgeniy', 'Vakulenko', 'your_gender')
 best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Mentor('Some', 'Buddy')
+
+best_lecturer = Lecturer('Ivan', 'Ivanov')
+best_lecturer.courses_attached += ['Python']
+
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
- 
+
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+
+best_student.rate_hw(best_lecturer, 'Python', 10)
+
+print(f'Оценки студента {best_student.name} {best_student.surname}: {best_student.grades}')
+print(f'Оценки лектора {best_lecturer.name} {best_lecturer.surname}: {best_lecturer.grades}')
