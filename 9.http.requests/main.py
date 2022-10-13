3b1c51e7e27f7eb70a01e60331362e137517d469
Evgeniy Vakulenko <vakevg@yandex.ru>
2022-10-13 19:32:23 +0300

    modify main.py

--- 9.http.requests/main.py
+++ 9.http.requests/main.py
@@ -13,7 +13,9 @@ def test_request():
         hero_intelligence = {f'{el["name"]}': f'{el["powerstats"]["intelligence"]}' for el in res}
         my_hero_intelligence = {f'{hero}': f'{hero_intelligence[hero]}' for hero in my_hero}
         sorted_my_hero = sorted(my_hero_intelligence.items(), key=lambda x: int(x[1]), reverse=True)
-    nprint(sorted_my_hero[0][0], ' - самый умный герой среди героев: '+', '.join(f'{hero}' for hero in my_hero))
+        nprint(sorted_my_hero[0][0], ' - самый умный герой среди героев: '+', '.join(f'{hero}' for hero in my_hero))
+    else:
+        print('Ошибка сервера:',response)
 
 if __name__ == "__main__":
     test_request()
