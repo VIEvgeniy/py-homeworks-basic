4608f22b18d512f5abb00c292006a8f4901f1a06
Evgeniy Vakulenko <vakevg@yandex.ru>
2022-10-18 13:59:50 +0300

    add main.py

---
+++ 9.http.requests/main.py
@@ -0,0 +1,19 @@
+import datetime
+import requests
+
+url = 'https://api.stackexchange.com/2.3/questions'
+date = datetime.date(datetime.date.today().year, datetime.date.today().month, datetime.date.today().day-1)
+
+params = {
+    'site': 'stackoverflow',
+    'fromdate': date
+}
+res = requests.get(url=url, params=params).json()
+quests = list(q for q in res['items'])
+python_quests = []
+
+for q in quests:
+    if 'python' in q['tags']:
+        python_quests.append(q)
+
+print('\n'.join(f'{quest["title"]}' for  quest in python_quests))
